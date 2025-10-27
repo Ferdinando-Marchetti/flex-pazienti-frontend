06ad89cb20de231ce864bc0188befe177c3e2c1a
Ferdinando Marchetti <marchettiferdinando@gmail.com>
2025-10-27 11:20:40 +0100

    aggiunto login

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,15 @@
 import { createBrowserRouter, Navigate } from "react-router-dom"
 
 import App from "./App"
-import AppLayout from "./pages/AppLayout"
-import DashboardPage from "./pages/DashboardPage"
-import ChatPage from "./pages/Chat"
+import AppLayout from "./pages/app/AppLayout"
+import DashboardPage from "./pages/app/DashboardPage"
+import ChatPage from "./pages/app/Chat"
+import LoginPage from "./pages/Login"
 
 export const router = createBrowserRouter([
   { path: '', element: <App />, children:[
-    { index: true, element: <Navigate to="/" replace /> },
-    
+    { index: true, element: <Navigate to="/login" replace /> },
+    { path: '/login', element: <LoginPage /> },
     { path: "/app", children:[
       { path: '', element: <AppLayout />, children:[
         { index: true, element: <Navigate to="dashboard" replace /> },
